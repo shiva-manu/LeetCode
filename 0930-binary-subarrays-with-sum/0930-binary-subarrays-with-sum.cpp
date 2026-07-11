4f30ddd9d46d7ce7ad9bb6814c0c1eabd2a6ec02
Shivamani <150359179+shiva-manu@users.noreply.github.com>
2026-07-11 13:53:27 +0530

    Time: 0 ms (100%), Space: 32.5 MB (82.95%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -2,17 +2,41 @@ class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
         int n=nums.size();
-        unordered_map<int,int> mp;
         int result=0;
-        int cummSum=0;
-        mp[0]=1; // I have seen CummSum=0 once
-        for(int &num: nums){
-            cummSum+=num;
-            int remaining_sum=cummSum-goal;
-            if(mp.find(remaining_sum)!=mp.end()){
-                result+=mp[remaining_sum];
+
+        //Approach-1
+        // unordered_map<int,int> mp;
+        // int cummSum=0;
+        // mp[0]=1; // I have seen CummSum=0 once
+        // for(int &num: nums){
+        //     cummSum+=num;
+        //     int remaining_sum=cummSum-goal;
+        //     if(mp.find(remaining_sum)!=mp.end()){
+        //         result+=mp[remaining_sum];
+        //     }
+        //     mp[cummSum]+=1;
+        // }
+
+        //Approach-2
+        int window=0;
+        int i=0,j=0;
+        int count_zero=0;
+        while(j<n){
+            window+=nums[j];
+            while(i<j && (nums[i]==0 || window>goal)){
+                
+                if(nums[i]==0){
+                    count_zero+=1;
+                }else{
+                    count_zero=0;
+                }
+                window-=nums[i];
+                i++;
             }
-            mp[cummSum]+=1;
+            if(window==goal){
+                result+=1+count_zero;
+            }
+            j++;
         }
         return result;
     }

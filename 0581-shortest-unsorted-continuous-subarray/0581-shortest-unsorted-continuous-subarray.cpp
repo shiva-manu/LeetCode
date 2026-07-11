d3036b8c4f5ea5b26a93afb3c982908e5a7314eb
Shivamani <150359179+shiva-manu@users.noreply.github.com>
2026-07-12 00:35:57 +0530

    Time: 3 ms (36.9%), Space: 31.7 MB (30.88%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
@@ -6,8 +6,9 @@ public:
         sort(sorted.begin(),sorted.end());
         int left=0;
         while(left<n && nums[left]==sorted[left]) left++;
+        if(left==n) return 0;
         int right=n-1;
-        while(right>=left && nums[right]==sorted[right]) right--;
+        while(right>=0 && nums[right]==sorted[right]) right--;
         return right-left+1;
     }
 };

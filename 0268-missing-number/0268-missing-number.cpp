48a6c2b89bc52b84a82b872b9da9fe93e9c44a25
Shivamani <150359179+shiva-manu@users.noreply.github.com>
2026-07-19 19:46:56 +0530

    Time: 19 ms (7.55%), Space: 21.8 MB (38.92%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int sum=0;
-        for(int num: nums){
-            sum+=num;
+        //Easy Approach to solve this
+        // int sum=0;
+        // for(int num: nums){
+        //     sum+=num;
+        // }
+        // int actualSum=0;
+        // for(int i=1;i<=nums.size();i++){
+        //     actualSum+=i;
+        // }
+        // return actualSum-sum;
+
+        //Approach -2 using the binary search
+        int low =0,high=nums.size()-1;
+        sort(nums.begin(),nums.end());
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==mid) low=mid+1;
+            else high=mid-1;
         }
-        int actualSum=0;
-        for(int i=1;i<=nums.size();i++){
-            actualSum+=i;
-        }
-        return actualSum-sum;
+        return low;
     }
 };

89a06b2160ba28d8f37ee5fa7aa21c0cc1d82c27
Shivamani <150359179+shiva-manu@users.noreply.github.com>
2026-07-21 13:04:28 +0530

    Time: 2337 ms (5.69%), Space: 77.4 MB (26.54%) - LeetHub

--- 1785-minimum-elements-to-add-to-form-a-given-sum/1785-minimum-elements-to-add-to-form-a-given-sum.cpp
+++ 1785-minimum-elements-to-add-to-form-a-given-sum/1785-minimum-elements-to-add-to-form-a-given-sum.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
     int minElements(vector<int>& nums, int limit, int goal) {
+        //Approach 1 brute force code little
+
+        // long long sum=0;
+        // int count=0;
+        // for(int num : nums){
+        //     sum+=num;
+        // }
+        // if(goal<0){
+        //     sum+=abs(goal);
+        //     while(sum>0){
+        //         sum=sum-limit;
+        //         count+=1;
+        //     }
+        // }else{
+        //     sum=(abs((long long) goal-sum));
+        //     while(sum>0){
+        //         sum=sum-limit;
+        //         count+=1;
+        //     }
+        // }
+        // return count;
+
         long long sum=0;
         int count=0;
-        for(int num : nums){
+        for(int num: nums){
             sum+=num;
         }
-        if(goal<0){
-            sum+=abs(goal);
-            while(sum>0){
-                sum=sum-limit;
-                count+=1;
-            }
-        }else{
-            sum=(abs((long long) goal-sum));
-            while(sum>0){
-                sum=sum-limit;
-                count+=1;
-            }
+        sum=abs(sum-goal);
+        while(sum>0){
+            sum-=limit;
+            count+=1;
         }
         return count;
     }

c95cf579c7fe785c8c56aa44a2f9893895eee0f3
Shivamani <150359179+shiva-manu@users.noreply.github.com>
2026-07-22 12:04:03 +0530

    Time: 0 ms (100%), Space: 13 MB (29.33%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        unordered_set<int> set;
-        for(int num: nums){
-            set.insert(num);
+        //Approach one using the hashset
+        // unordered_set<int> set;
+        // for(int num: nums){
+        //     set.insert(num);
+        // }
+        // vector<int> result;
+        // for(auto &it: set){
+        //     result.push_back(it);
+        // }
+        // sort(result.begin(),result.end());
+        // int n=result.size();
+        // if(n==1){
+        //     return result[0];
+        // }else if(n==2){
+        //     return result[1];
+        // }
+        // return result[n-3];
+
+        //Approach two processing the array;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.end());
+        int count=1;
+        for(int i=1;i<n;i++){
+            if(nums[i-1]==nums[i]) continue;
+            count+=1;
+            if(count==3) return nums[i];
         }
-        vector<int> result;
-        for(auto &it: set){
-            result.push_back(it);
-        }
-        sort(result.begin(),result.end());
-        int n=result.size();
-        if(n==1){
-            return result[0];
-        }else if(n==2){
-            return result[1];
-        }
-        return result[n-3];
+        return nums[0];
     }
 };

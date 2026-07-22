5191a766ce5cf24274f5af1b2fa1d16465c80127
Shivamani <150359179+shiva-manu@users.noreply.github.com>
2026-07-22 08:25:01 +0530

    Time: 16 ms (13.95%), Space: 19.7 MB (28.86%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.cpp
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.cpp
@@ -3,11 +3,20 @@ public:
     int minOperations(vector<int>& nums) {
         int count=0;
         int n=nums.size();
-        int prev=nums[0];
+        //Approach 1 simulation(greedy approach)
+        // for(int i=1;i<n;i++){
+        //     if(nums[i-1]>=nums[i]){
+        //         count+=1;
+        //         nums[i]+=1;
+        //     }
+        // }
+
+        //Greedy approach with optimization
         for(int i=1;i<n;i++){
-            while(nums[i-1]>=nums[i]){
-                count+=1;
-                nums[i]+=1;
+            if(nums[i]<=nums[i-1]){
+                int diff=nums[i-1]-nums[i]+1;
+                count+=diff;
+                nums[i]+=diff;
             }
         }
         return count;

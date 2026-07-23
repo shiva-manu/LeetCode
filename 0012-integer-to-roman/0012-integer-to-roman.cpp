50bf6165a1ca1a3ff76d8dbd300f0ddd78d746cb
Shivamani <150359179+shiva-manu@users.noreply.github.com>
2026-07-23 19:59:47 +0530

    Time: 19 ms (5.35%), Space: 14.8 MB (5.43%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string getRoman(int num){
+        unordered_map<int, string> map = {
+            {1000, "M"},
+            {900, "CM"},
+            {500, "D"},
+            {400, "CD"},
+            {100, "C"},
+            {90, "XC"},
+            {50, "L"},
+            {40, "XL"},
+            {10, "X"},
+            {9, "IX"},
+            {5, "V"},
+            {4, "IV"},
+            {1, "I"}
+        };
+        string result="";
+        vector<int> values={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        for(int value: values){
+            while(num>=value){
+                result+=map[value];
+                num-=value;
+            }
+        }
+        return result;
+    }
+    string intToRoman(int num) {
+        return getRoman(num);
+    }
+};

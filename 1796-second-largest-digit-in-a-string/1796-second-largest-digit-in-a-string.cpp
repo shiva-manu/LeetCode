ce9eef92eec2f45fcaf74b6e746051ebefb9942b
Shivamani <150359179+shiva-manu@users.noreply.github.com>
2026-07-29 21:16:10 +0530

    Time: 0 ms (100%), Space: 9.5 MB (40.94%) - LeetHub

--- 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.cpp
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int secondHighest(string s) {
-        unordered_set<int> set;
-        for(char ch : s){
-            if(!isalpha(ch)){
-                set.insert(ch-'0');
-            }
-        }
         int maxi1=-1,maxi2=-1;
-        for(auto &it: set){
-            if(it>maxi1){
-                maxi2=maxi1;
-                maxi1=it;
-            }else if(it>maxi2){
-                maxi2=it;
+        for(char ch: s){
+            if(!isalpha(ch)){
+                if(ch-'0'>maxi1){
+                    maxi2=maxi1;
+                    maxi1=ch-'0';
+                }else if( ch-'0'!=maxi1 && ch-'0'>maxi2){
+                    maxi2=ch-'0';
+                }
             }
         }
         return maxi2;

9cc1d883fac2b06ed505f485b10f4a02cb4a65a3
Shivamani <150359179+shiva-manu@users.noreply.github.com>
2026-07-29 08:30:34 +0530

    Time: 16 ms (94.58%), Space: 80.7 MB (13.23%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     string smallestPalindrome(string s) {
         int n=s.size();
-        stack<string> st;
         vector<int> freq(26,0);
         for(char ch: s){
             freq[ch-'a']+=1;
@@ -13,17 +12,14 @@ public:
         for(int i=0;i<26;i++){
             // Put half of the occurrences in the left half
             left+=string(freq[i]/2,'a'+i);
-            string temp=string(freq[i]/2,'a'+i);
-            st.push(temp);
+
             //If odd frequency keep one character for the middle
             if(freq[i]%2==1){
                 mid=char('a'+i);
             }
         }
-        while(!st.empty()){
-            right+=st.top();
-            st.pop();
-        }
+        right=left;
+        reverse(right.begin(),right.end());
         return left+mid+right;
     }
 };

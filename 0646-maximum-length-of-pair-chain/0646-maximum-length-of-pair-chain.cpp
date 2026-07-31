16aeb3aeb4e74dcf1cc63249662c50985f20b572
Shivamani <150359179+shiva-manu@users.noreply.github.com>
2026-07-31 11:44:57 +0530

    Time: 166 ms (24.32%), Space: 31.1 MB (20.54%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -17,7 +17,20 @@ public:
         return ans;
 
     }
+    int dp[1001][1001];
+    int dpMemoization(vector<vector<int>>& pairs,int idx,int prevIdx){
+        int take=0;
+        if(idx==pairs.size()) return 0;
+        if(prevIdx!=-1 && dp[prevIdx+1][idx]!=-1) return dp[prevIdx+1][idx];
+        if(prevIdx==-1 || pairs[prevIdx][1]< pairs[idx][0]){
+            take=1+dpMemoization(pairs,idx+1,idx);
+        }
+        int skip=dpMemoization(pairs,idx+1,prevIdx);
+        return dp[prevIdx+1][idx]=max(take,skip);
+    }
     int findLongestChain(vector<vector<int>>& pairs) {
-        return greedy(pairs);
+        sort(pairs.begin(),pairs.end());
+        memset(dp,-1,sizeof(dp));
+        return dpMemoization(pairs,0,-1);
     }
 };

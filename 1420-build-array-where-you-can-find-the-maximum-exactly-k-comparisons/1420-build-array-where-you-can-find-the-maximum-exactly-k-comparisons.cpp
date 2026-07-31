116f3d0fd89f9c8be96c13d33483bbcd8a3702ab
Shivamani <150359179+shiva-manu@users.noreply.github.com>
2026-07-31 17:12:36 +0530

    Time: 140 ms (43.61%), Space: 8.9 MB (94.64%) - LeetHub

---
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int N,M,K;
+    int MOD=1e9+7;
+    int dp[51][51][101];
+    int solve(int idx,int searchCost,int  maxCostSoFor){
+        if(idx==N){
+            if(searchCost==K) return 1;
+            return 0;
+        }
+        if(dp[idx][searchCost][maxCostSoFor]!=-1) return dp[idx][searchCost][maxCostSoFor];
+        int result=0;
+        for(int i=1;i<=M;i++){
+            if(i>maxCostSoFor){
+                result=(result+solve(idx+1,searchCost+1,i))%MOD;
+            }else{
+                result=(result+solve(idx+1,searchCost,maxCostSoFor))%MOD;
+            }
+        }
+        return dp[idx][searchCost][maxCostSoFor]=result%MOD;
+    }
+    int numOfArrays(int n, int m, int k) {
+        memset(dp,-1,sizeof(dp));
+        N=n;
+        M=m;
+        K=k;
+        return solve(0,0,0);
+    }
+};

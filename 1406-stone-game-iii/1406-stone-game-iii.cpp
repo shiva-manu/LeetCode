2d71aab542051e581ad249b4edbd06947bc9302d
Shivamani <150359179+shiva-manu@users.noreply.github.com>
2026-08-03 14:42:08 +0530

    Time: 12 ms (86.94%), Space: 136.5 MB (61.8%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -10,10 +10,20 @@ public:
         if(idx+2<n) result=max(result,nums[idx]+nums[idx+1]+nums[idx+2]-dpMemoization(nums,idx+3)); 
         return dp[idx]=result;
     }
+    int dpButtonUp(vector<int>& nums){
+        int n=nums.size();
+        vector<int> t(n+1);
+        for(int i=n-1;i>=0;i--){
+            t[i]=nums[i]-t[i+1];
+            if(i+1<n) t[i]=max(t[i],nums[i]+nums[i+1]-t[i+2]);
+            if(i+2<n) t[i]=max(t[i],nums[i]+nums[i+1]+nums[i+2]-t[i+3]);
+        }
+        return t[0];
+    }
     string stoneGameIII(vector<int>& stoneValue) {
         memset(dp,-1,sizeof(dp));
         n=stoneValue.size();
-        int result=dpMemoization(stoneValue,0);
+        int result=dpButtonUp(stoneValue);
         if(result==0) return "Tie";
         else if(result<0) return "Bob";
         else return "Alice";

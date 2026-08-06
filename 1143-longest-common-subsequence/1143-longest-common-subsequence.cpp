f5b07f42eab975fac974cc79cce20d4ca1f26f53
Shivamani <150359179+shiva-manu@users.noreply.github.com>
2026-08-06 21:52:24 +0530

    Time: 34 ms (35.16%), Space: 13.1 MB (83.51%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -4,7 +4,7 @@ public:
     int dpMemoization(string& s1,string& s2,int i,int j){
         if(i>=s1.size() || j>=s2.size()) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(s1[i]==s2[j]) return dp[i][j]=1+dpMemoization(s1,s2,i+1,j+1);
+        if(s1[i]==s2[j]) return 1+dpMemoization(s1,s2,i+1,j+1);
         else return dp[i][j]=max(dpMemoization(s1,s2,i,j+1),dpMemoization(s1,s2,i+1,j));
     }
     int longestCommonSubsequence(string text1, string text2) {

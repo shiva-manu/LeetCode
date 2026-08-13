e147c25ecf163cf64fd5efa0b948692ff8e1734b
Shivamani <150359179+shiva-manu@users.noreply.github.com>
2026-08-13 09:43:27 +0530

    Time: 26 ms (77.76%), Space: 11.4 MB (89.33%) - LeetHub

---
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int result=INT_MIN;
+    int m;
+    void backTrack(int idx,int count,int n,vector<int>& resultant,vector<vector<int>>& requests){
+        if(idx>=m){
+            bool allZero=true;
+            for(int &x: resultant){
+                if(x!=0){
+                    allZero=false;
+                    break;
+                }
+            }
+            if(allZero==true){
+                result=max(result,count);
+            }
+            return;
+        }
+        int from=requests[idx][0];
+        int to=requests[idx][1];
+        resultant[from]--;
+        resultant[to]++;
+        backTrack(idx+1,count+1,n,resultant,requests);
+        resultant[from]++;
+        resultant[to]--;
+        backTrack(idx+1,count,n,resultant,requests);
+    }
+    int maximumRequests(int n, vector<vector<int>>& requests) {
+          m=requests.size();
+          vector<int> resultant(n,0);
+          backTrack(0,0,n,resultant,requests);
+          return result;
+    }
+};

5e1a51c48f2ad343ea9e3e64a2a31a4bcfaca26a
Shivamani <150359179+shiva-manu@users.noreply.github.com>
2026-08-14 17:33:02 +0530

    Time: 0 ms (100%), Space: 14.8 MB (65.74%) - LeetHub

--- 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.cpp
+++ 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int findLengthOfLCIS(vector<int>& nums) {
         int n=nums.size();
+        if(n==0) return 0;
         int mx=0,count=0;
         int prev=nums[0];
         for(int i=1;i<n;i++){

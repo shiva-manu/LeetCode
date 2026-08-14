66d515b1ed56a13b8d8f78ee4073c953b2c243fb
Shivamani <150359179+shiva-manu@users.noreply.github.com>
2026-08-14 18:02:55 +0530

    Time: 0 ms (100%), Space: 14 MB (13.82%) - LeetHub

--- 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.cpp
+++ 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int dominantIndex(vector<int>& nums) {
+    int hashMapAndSort(vector<int>& nums){
         unordered_map<int,int> hashMap;
         int n=nums.size();
         for(int i=0;i<n;i++){
@@ -12,5 +12,20 @@ public:
             if(2*nums[i]>mx) return -1;
         }
         return hashMap[mx];
+    }
+    int dominantIndex(vector<int>& nums) {
+        int n=nums.size();
+        int maxValue=-1,secondMax=-1,maxIdx=-1;
+        for(int i=0;i<n;i++){
+            if(nums[i]>maxValue){
+                secondMax=maxValue;
+                maxValue=nums[i];
+                maxIdx=i;
+            }else if(nums[i]>secondMax){
+                secondMax=nums[i];
+            }
+        }
+        if(2*secondMax>maxValue)return -1;
+        return maxIdx;
     }
 };
